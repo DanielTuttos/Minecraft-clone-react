9562291cfbf7d5adeabfd97509b23d101a56ba0e
Daniel Romero <tutorialesdanielromero@gmail.com>
2024-01-17 14:17:28 -0500

    change bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { Physics } from '@react-three/cannon';
 import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import Ground from './components/Ground';
-import Fpv from './components/Fpv.JSX';
 import Player from './components/Player';
 import Cubes from './components/Cubes';
 import TextureSelect from './components/TextureSelect';
+import Fpv from './components/Fpv';
 
 function App() {
   return (
